775f3ed984a01cba90c957bd97b20b7dc8626ad0
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-10-30 22:51:19 +0530

    To print 1 to 100 numbers

---
+++ while_loop.c
@@ -0,0 +1,11 @@
+// print 1 to 100 numbers using while loop
+#include<stdio.h>
+int main(){
+    int i=1// initialize the value
+    while(i<=100){
+        printf("%d",i);
+        i=i+1 // updating the iterating variable
+    }
+
+return 0;
+}
