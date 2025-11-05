6bfb248b20d1d3934992066d6e2c4342d3441fc4
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-06 00:10:33 +0530

    Add files via upload

---
+++ AREA.C
@@ -0,0 +1,16 @@
+	#include<stdio.h>
+
+	// area of square
+	int main(){
+	int side;
+	printf("Enter side : ");
+	scanf("%d",&side);
+	printf("The area of square is :",side*side);
+
+	// area of circle
+	int radius;
+	printf("Enter radius :");
+	scanf("%d",&radius);
+	printf("The area of circle is :",3.14*radius*radius);
+	return 0;
+	}
