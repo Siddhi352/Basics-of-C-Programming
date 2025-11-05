467f5eb61a70c7cdc144f02c4413b8d56b2e4594
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-06 00:43:40 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int num1 =3; //INTEGER Variable
+    float num2=4.5; //FLOATING-POINT Variable
+    char letter = 'S'; // CHARACTER Variable
+    
+    return 0;
+}
