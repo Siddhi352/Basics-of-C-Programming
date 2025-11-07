e9898dbb58e64f32e5d481e72d30d42aeb2f56ce
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-07 23:37:47 +0530

    instructions in c

---
+++ instruction.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    // These types of declaration we can do in c.
+    int a = 12;
+    int b = a;
+    int c = b + 1;
+    int d = 1, e;
+    int power = pow(a, c);
+    printf("%d", power);
+    printf("%d", power); // This will print power of a and c.
+    return 0;
+}
