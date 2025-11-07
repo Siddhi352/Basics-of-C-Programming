9957fd474157d635415a1c7e8a847655e503d231
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-08 01:26:25 +0530

    unconditional statement in c

---
+++ continue.c
@@ -0,0 +1,17 @@
+    #include<stdio.h>
+
+    int main()
+    {
+        int i;
+        for(i=0;i<=20;i++)
+        {
+            printf("%d ",i);
+        
+        if(i==18)
+        {
+            continue;
+    
+        }
+        }   
+
+    }
