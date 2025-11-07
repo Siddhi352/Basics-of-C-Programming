d21984b14427f95b31245d6501a395bd69394d02
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-07 19:58:05 +0530

    Add files via upload

---
+++ 1_forloop.c
@@ -0,0 +1,13 @@
+// to print name multiple times in a loop
+    #include<stdio.h>
+    
+    int main(){
+        int initialization;
+        
+        for(initialization=1;initialization<=10;initialization++)
+        {
+            printf("\nSIDDHI");
+        }
+        
+        return 0;
+    }
