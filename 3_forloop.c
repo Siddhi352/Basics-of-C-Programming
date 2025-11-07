3a1d9cdd1f65154ec6a094f65ad60f819df4abca
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-07 20:51:43 +0530

    Add files via upload

---
+++ 3_forloop.c
@@ -0,0 +1,13 @@
+// to print numbers in reverse order using loop
+    #include<stdio.h>
+
+    int main()
+    {
+        int i;
+        for(i=20;i>=1;i--)
+        {
+            printf("%d\n",i);
+        }
+            printf("\n i value at the end : %d",i);
+            return 0;
+    }
