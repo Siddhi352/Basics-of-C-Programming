1d6278dd02ad7fa7fc9f24c6f4593f57d32c57a2
Siddhi352 <siddhi.jadhav1507@gmail.com>
2025-11-08 01:00:50 +0530

    Infinite loop in c

---
+++ 1_loop.c
@@ -0,0 +1,12 @@
+// to run infinite loop till range
+    #include<stdio.h>
+    
+    int main()
+    {
+        int i;
+        for(i=1;i++;)
+        {
+            printf("\n%d");
+        }
+        return 0;
+    }
